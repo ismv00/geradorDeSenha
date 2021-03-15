487ebcee6151d79031341ee02a6947f9888bc262
Igor Silva <ismv00@icloud.com>
2021-03-14 21:07:10 -0400

    AppFinalizado

--- App.js
+++ App.js
@@ -1,21 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Image, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import ClipBoard from 'expo-clipboard';
+
+import Slider from '@react-native-community/slider';
+
+import Logo from './src/assets/logo.png';
+
+let charset= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 export default function App() {
-  return (
+
+  const [password, setPassword] = useState('');
+  const [slider, setSlider] = useState(5);
+
+  function generatePass(){
+    let pass = '';
+
+    for(let i = 0, n = charset.length; i < slider; i++){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    setPassword(pass);
+  }
+
+  function copyToClipBoard(){
+    ClipBoard.setString(password);
+    alert('Password copied sucessfull!')
+  }
+
+  
+  
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={Logo} />
+      <Text style={styles.title}>{slider} Caracteres</Text>
+
+      <View style={styles.contentSlider}>
+        <Slider 
+          style={{ width: 300, heigth: 40}}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#FF1408"
+          maximumTrackTintColor="#0C2576"
+          value={slider}
+          onValueChange={(valueSlider) => setSlider(valueSlider.toFixed(0))}
+        /> 
+      </View> 
+
+      <TouchableOpacity style={styles.button}onPress={generatePass}>
+        <Text style={styles.textButton}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      <View style={styles.password}>
+        <Text style={styles.textPass} onLongPress={copyToClipBoard}>{password}</Text>
+      </View>      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#0C2576',
+    flex: 1, 
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  title: {
+    marginTop: 20,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  contentSlider: {
+    backgroundColor: '#FFF',
+    padding: 20,
+    justifyContent: 'center',
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#D67D1E',
+
+    width: 340,
+    height: 65,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    borderRadius: 20,
+  },
+
+  textButton: {
+    color: '#FFF',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  password: {
+    width: 340,
+    height: 65,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    borderRadius: 20,
+  },
+
+  textPass: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#0C2576'
+  }
+})
---
+++ useCores.md
@@ -0,0 +1,48 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0C2576'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: '#D67D1E'
+  },
+  contenteSlider: {
+    backgroundColor: '#FFF3EA',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D67D1E',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 20,
+    width: 320,
+    height: 55
+  },
+  password: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF3EA',
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 20,
+    width: 320,
+    height: 55    
+  },
+  textPassword: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
+
+
+ minimumTrackTintColor={'#FF1408'}
+          maximumTrackTintColor={'#0C2576'}  
